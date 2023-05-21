382a07a220f263c0ce1af90a4809116323707197
Andy <65276814+yandhii@users.noreply.github.com>
2023-05-22 07:27:51 +0800

    Update Enum.sol

--- Enum.sol
+++ Enum.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
 
+contract Enum{
+    enum Status{
+        None,
+        shipped,
+        finished
+    }
+
+    Status public status;
+
+    function get() public view returns (Status){
+        return status;
+    }
+
+    function set(Status _status) external{
+        status = _status;
+    }
+
+    function reset() external{
+        delete status;
+    }
+
+    function ship() external{
+        status = Status.shipped;
+    }
+}
